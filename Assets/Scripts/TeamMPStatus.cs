dc712fd01377d363ef86297ddbee4b993dcc853c
Plumes1227 <qfbnhg56548x@gmail.com>
2023-12-15 16:14:52 +0800

    feat: MPUI

---
+++ Assets/Scripts/TeamMPStatus.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class TeamMPStatus : MonoBehaviour
+    {
+        [SerializeField] private Image image;
+        [SerializeField] private Button plusButton;
+        [SerializeField] private Button minusButton;
+
+        private int mp;
+        private int TopMP;
+
+        private void Start()
+        {
+            plusButton.onClick.AddListener(AddScore);
+            minusButton.onClick.AddListener(DeductedScore);
+        }
+        
+        private void AddScore()
+        {
+            mp++;
+            UpdateTopScore();
+            image.fillAmount = mp / TopMP;
+        }
+
+        private void UpdateTopScore()
+        {
+            if (mp > TopMP) TopMP = mp;
+        }
+
+        private void DeductedScore()
+        {
+            mp--;
+            UpdateTopScore();
+            image.fillAmount = mp / TopMP;
+        }
+    }
+}
